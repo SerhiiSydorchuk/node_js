6c169b7131ddabfe20de5695f1b78f71f2162602
SerhiiSydorchuk <sydorchuks93@gmail.com>
2024-12-03 17:20:28 +0100

    first

---
+++ helpers/helper.js
@@ -0,0 +1,5 @@
+console.log('____helper_____');
+
+console.log(__dirname);
+console.log(__filename);
+
---
+++ main.js
@@ -0,0 +1,7 @@
+require('./helpers/helper')
+
+console.log('____main.js_____');
+console.log(__dirname);
+
+console.log(__filename);
+
